5b30da0feaba01cb868fdadbdbb6d782ab86c300
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-23 21:42:56 -0700

    Add age field

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -5,4 +5,4 @@ class Person(models.Model):
     Person_name = models.CharField(max_length=20)
     email = models.EmailField()
     phone = models.CharField(max_length=20)
-    
+    age = models.IntegerField()
